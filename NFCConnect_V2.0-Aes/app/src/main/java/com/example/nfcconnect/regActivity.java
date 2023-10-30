02978d052c69d6f8de24eac0c613bbf7bfd1e1fc
Samarth Amodkar <88137537+samarth23-tech23@users.noreply.github.com>
2023-10-30 23:03:11 +0530

    Update regActivity.java

--- NFCConnect_V2.0-Aes/app/src/main/java/com/example/nfcconnect/regActivity.java
+++ NFCConnect_V2.0-Aes/app/src/main/java/com/example/nfcconnect/regActivity.java
@@ -1,6 +1,7 @@
 package com.example.nfcconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
@@ -12,12 +13,21 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.io.IOException;
 import java.util.Base64; // Import Base64
 import java.util.HashMap;
 import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class regActivity extends AppCompatActivity implements TextWatcher {
 
@@ -28,6 +38,7 @@ public class regActivity extends AppCompatActivity implements TextWatcher {
     String encPassword, encNFCpass, name, emailId, aesKeyString; // Store the AES key as a string
     SecretKey aesKey;
     ProgressBar progressBar;
+    OkHttpClient okHttpClient = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +69,7 @@ public class regActivity extends AppCompatActivity implements TextWatcher {
                 emailId = regemail.getText().toString();
                 String password = regPass.getText().toString();
                 String NFC_Password = regnPass.getText().toString();
-              
+
                 if (name.length() != 0 && emailId.length() != 0 && password.length() != 0 && NFC_Password.length() != 0) {
                     try {
                         // Use the same AES key for both password and NFC password encryption
@@ -89,6 +100,55 @@ public class regActivity extends AppCompatActivity implements TextWatcher {
                 } else {
                     Toast.makeText(regActivity.this, "Fill all the required fields", Toast.LENGTH_SHORT).show();
                 }
+
+                String combinedUP = name + ":" + encNFCpass;
+                //sending to raspberry pi
+                // For resetting, send combinedUP to the server (e.g., via HTTP request)
+                RequestBody formbody4 = new FormBody.Builder()
+                        .add("password", combinedUP)
+                        .add("activity", "register")
+                        .build();
+
+                Request request3 = new Request.Builder()
+                        .url("https://raspi-nfcapi23.socketxp.com/register")
+                        .post(formbody4)
+                        .build();
+
+                Toast.makeText(regActivity.this, "Sending to Raspberry PI", Toast.LENGTH_SHORT).show();
+
+                okHttpClient.newCall(request3).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getApplicationContext(), "Socket response error", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onResponse(Call call, okhttp3.Response response) throws IOException {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Handle the response from the Raspberry Pi
+                                try {
+                                    String responseText = response.body().string();
+                                    // Assuming that responseText is the UID you want to associate with the user
+                                    if (!responseText.isEmpty()) {
+                                        // Push the UID into the user's data in the Firebase Realtime Database
+                                        DatabaseReference userReference = reference.child(name); // Assuming 'name' is the user's name
+                                        userReference.child("uid").setValue(responseText);
+                                        Toast.makeText(getApplicationContext(), "Raspberry Pi Response: " + responseText, Toast.LENGTH_LONG).show();
+                                    }
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    }
+                });
             }
         });
 
